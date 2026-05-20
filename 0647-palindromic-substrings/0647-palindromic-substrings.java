6aafa5cec1c5fd78928df40cb109e9fc8f83d81f
Pathapalem RamKumar <rpathapalem@gmail.com>
2026-05-20 21:52:29 +0530

    Time: 673 ms (15.87%), Space: 42.7 MB (89.95%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(isPal(s,i,j)) count++;
+            }
+        }
+        return count;
+    }
+    public boolean isPal(String s, int l,int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
