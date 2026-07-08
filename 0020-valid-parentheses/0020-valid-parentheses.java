d7681824686ef9540eb199e0d1e8a1ffc1a8ece0
Pathapalem RamKumar <rpathapalem@gmail.com>
2026-07-08 19:05:57 +0530

    Time: 3 ms (87.19%), Space: 43.2 MB (57.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch=='{' ||ch=='[' || ch=='(') st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                char t= st.pop();
+                if(t != '{' && ch=='}' || t!='[' && ch==']' || t!='(' && ch==')') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
